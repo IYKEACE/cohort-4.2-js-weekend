7930dcfc7abbb4556cdde21b159d3b5be2b9cc6e
iykeace <ikechukwuu338@gmail.com>
2024-12-02 13:12:13 +0400

    HELLO JAVASCRIPT

---
+++ 1-datatype.js
@@ -0,0 +1,22 @@
+console.log("david".length);
+console.log("iyke".toUpperCase());
+console.log("All character to lowercase =>","I AM SUPER EXCITED TO START LEARNING JS".toLowerCase)
+console.log("check whether it contain a substring =>", "david".includes("d"));// true
+console.log("check whether it contain a substring =>", "david".includes("o"));// false
+
+console.log("first character", "david"[0]); // a
+console.log("first character", "david"[1]); // b
+console.log("first character", "david"[2]); // c
+console.log("first character", "david"[3]); // d
+console.log("first character", "david"[4]); // e
+
+console.log("david".slice(0,3))
+
+console.log("david".split(""))
+
+console.log("Math random result btw 0 and 1 =>",Math.random());
+console.log("Math random result btw 0 and 5 =>",Math.random() * 5);
+console.log("Math random result numbers from 1 to 10 =>", Math.random() * 10 + 1);
+
+console.log("Math random result numbers from 1 to 100 =>",Math.random() *100 + 1 )
+console.log("RESULT OF RANDOM =>", Math.random() *100 + 1);
